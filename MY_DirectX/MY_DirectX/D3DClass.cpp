a8507b83a5c31583d578a7b110c8008542b43c86
PSDent <mnsan1004@gmail.com>
2017-10-06 04:39:12 +0900

    Update D3DClass.cpp

--- MY_DirectX/MY_DirectX/D3DClass.cpp
+++ MY_DirectX/MY_DirectX/D3DClass.cpp
@@ -35,46 +35,80 @@ bool D3DClass::InitD3D(HWND hwnd, int width, int height)
 	ID3D11Texture2D* pBackBuffer = NULL;
 	D3D11_VIEWPORT m_viewPort;
 
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
 	// Create SwapChain
 	ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
 	swapChainDesc.BufferCount = 1;
-	swapChainDesc.BufferDesc.Width = width/ 2;
-	swapChainDesc.BufferDesc.Height = height / 3;
+	swapChainDesc.BufferDesc.Width = width;
+	swapChainDesc.BufferDesc.Height = height;
 	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
 	swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
 	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapChainDesc.OutputWindow = hwnd;
-	swapChainDesc.SampleDesc.Count = 1; // Turn off Multisampling. count = 1 & Quality = 0;
+	swapChainDesc.SampleDesc.Count = 1;
 	swapChainDesc.SampleDesc.Quality = 0;
-	swapChainDesc.Windowed = TRUE;
-	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-	swapChainDesc.Flags = 0; // Don't set anything advanced flags.
+	swapChainDesc.Windowed = !FULL_SCREEN;
 
 	featureLevel = D3D_FEATURE_LEVEL_11_0;
 
-	// Create Device & SwapChain.
-	hr = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, &featureLevel, 1,
-		D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, NULL, &m_deviceContext);
-	
+	// Create Device & Swa+pChain.
+	for (UINT i = 0; i < numDriverTypes; i++)
+	{
+		D3D_DRIVER_TYPE t_driverType = driverTypes[i];
+
+		hr = D3D11CreateDeviceAndSwapChain(
+			NULL,			   // Adapter (Graphic Card)
+			t_driverType,      // Driver Type
+			NULL,			   // Driver Type Software. Usually set with NULL.
+			0,				   // Device Flag 
+			featureLevels,     // FeatureLevel Array
+			numFeatureLevels,  // FeatureLevel Count
+			D3D11_SDK_VERSION, // SDK Version
+			&swapChainDesc,    // Swapchain Description Struct
+			&m_swapChain,      // Created Swapchain Object
+			&m_device,         // Created Device Object
+			&featureLevel,     // Created FeatureLevel Information
+			&m_deviceContext   // Create DeviceContext Object
+		);
+
+		if (SUCCEEDED(hr))
+			break;
+	}
+
 	if (FAILED(hr))
 	{
-		MessageBox(g_hwnd, L"Failed Create Device and Swapchain.", L"Opps!", MB_OK);
+		MessageBox(hwnd, L"Failed Create Device and Swapchain.", L"Opps!", MB_OK);
 		return hr;
 	}
 
-	// Render Target View.
-	hr = m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& pBackBuffer);
+	// Get BackBuffer Pointer
+	hr = m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
 	if (FAILED(hr))
 	{
-		MessageBox(g_hwnd, L"Failed Get Swapchain Buffer.", L"Opps!", MB_OK);
+		MessageBox(hwnd, L"Failed Get Swapchain Buffer.", L"Opps!", MB_OK);
 		return hr;
 	}
 
 	hr = m_device->CreateRenderTargetView(pBackBuffer, NULL, &m_renderTargetView);
 	if (FAILED(hr))
 	{
-		MessageBox(g_hwnd, L"Failed Create Render Target View.", L"Opps!", MB_OK);
+		MessageBox(hwnd, L"Failed Create Render Target View.", L"Opps!", MB_OK);
 		return hr;
 	}
 
@@ -82,23 +116,16 @@ bool D3DClass::InitD3D(HWND hwnd, int width, int height)
 	pBackBuffer->Release();
 	pBackBuffer = 0;
 
-	m_deviceContext->OMGetRenderTargets(1, &m_renderTargetView, NULL);
+	m_deviceContext->OMSetRenderTargets(1, &m_renderTargetView, NULL);
 
 	// ViewPort
-	m_viewPort.Width = (FLOAT)width / 2;
-	m_viewPort.Height = (FLOAT)height / 3;
+	m_viewPort.Width = (FLOAT)width;
+	m_viewPort.Height = (FLOAT)height;
 	m_viewPort.MinDepth = 0.0f;
 	m_viewPort.MaxDepth = 1.0f;
-	m_viewPort.TopLeftX = width / 4;
-	m_viewPort.TopLeftY = height / 6;
-	m_deviceContext->RSSetViewports(1, &m_viewPort);
-	/*
-	screen.W / 4,  // Start posX
-		screen.H / 6,  // Start posY
-	*/
-
-	ClearScene();
-
+	m_viewPort.TopLeftX = 0;
+	m_viewPort.TopLeftY = 0;
+	m_deviceContext->RSSetViewports(1, &m_viewPort);	
 	return true;
 }
 
@@ -128,7 +155,7 @@ ID3D11DeviceContext* D3DClass::GetDeviceContext()
 
 void D3DClass::ClearScene()
 {
-	float color[4] = { 1.0f, 1.0f, 0.0f, 1.0f };
+	float color[4] = { 1.0f, 0.0f, 0.0f, 0.0f };
 
 	m_deviceContext->ClearRenderTargetView(m_renderTargetView, color);
 	m_swapChain->Present(0, 0);
